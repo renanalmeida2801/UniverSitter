ccecf66a7cb1c291c28d5d97c817c2087ec1ec33
Marcelo Mikael <marcelo.mplp@hotmail.com>
2024-08-04 18:32:48 -0300

    conectando o banco com a tela de cadastro

--- universitter/src/config/db.js
+++ universitter/src/config/db.js
@@ -1,22 +1,55 @@
 require('dotenv').config();
-const { Client } = require('pg');
+const cors = require('@fastify/cors');
+const Fastify = require('fastify');
+const fastify = Fastify({ logger: true });
 
-const cliente = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+fastify.register(require('@fastify/postgres'), {
+    connectionString: process.env.CONNECTIONSTRING
+});
 
-async function testar() {
-    try {
-        await cliente.connect();
-        console.log('conectado com sucesso')
-        console.log(await cliente.query('SELECT now()'));
-    } catch (error) {
-        console.error('Erro ao tentar executar: ', error)
+fastify.register(cors, {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+});
+
+fastify.post('/register', async (request, reply) => {
+
+    const data = request.body;
+
+    
+    if (Object.keys(data).length === 0) {
+        return reply.status(400).send({ status: 'error', message: 'Dados insuficientes' });
     }
-}
 
-testar();
+    
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+
+    
+    const placeholders = fields.map((_, index) => `$${index + 1}`).join(', ');
+
+    
+    const query = `INSERT INTO usuario (${fields.join(', ')}) VALUES (${placeholders})`;
+
+    try {
+        
+        const result = await fastify.pg.query(query, values);
+        reply.send({ status: 'success', data: result.rows });
+    } catch (err) {
+        fastify.log.error(err);
+        reply.status(500).send({ status: 'error', message: 'Erro ao registrar usuário' });
+    }
+});
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3300, host: 'localhost' });
+        fastify.log.info(`Servidor rodando em http://localhost:3300`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
