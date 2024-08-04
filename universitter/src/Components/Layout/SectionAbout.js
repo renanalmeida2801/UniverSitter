b47e167540b64dea2e7922623fb086ec8c124814
Renan Almeida <renanvictoralmeidasilva@gmail.com>
2024-08-04 00:40:18 -0300

    tela-home

---
+++ universitter/src/Components/Layout/SectionAbout.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+import styles from './SectionAbout.module.css'
+import image from '../../img/plantaQuemSomos.jpg'
+
+function SectionAbout() {
+    return (
+        <div className={styles.container}>
+            <div>
+                <h2 className={styles.text}>Quem somos?</h2><br></br>
+                <p className={styles.info}>O UniverSitter é um projeto de alunos da UFC - Quixadá</p><br></br>
+                <p className={styles.info}>Nossa ideia é conectar os unversitários do campus e fortalecer a comunidade através de uma troca de favores!</p><br></br>
+                <p className={styles.info}>Faça parte da família UniverSitter!</p>
+                <Link className={styles.button} to='/'>Encontrar um cuidador</Link>
+                <Link className={styles.button} to='/'>Tornar-se um cuidador</Link>
+
+            </div>
+            <div>
+                <img className={styles.img} src={image} alt='vaso com plantas'></img>
+            </div>
+        </div>
+    );
+}
+
+export default SectionAbout;
--- universitter/src/Components/Pages/Home.js
+++ universitter/src/Components/Pages/Home.js
@@ -1,14 +1,16 @@
-import UserForm from "../Form/UserForm";
 
-import Styles from './Home.module.css'
-
+import SectionHome from '../Layout/SectionHome';
+import SectionInfo from '../Layout/SectionInfo';
+import SectionAbout from '../Layout/SectionAbout';
 
 function Home(){
     return(
-        <div className={Styles.body}>
-            <UserForm btnText="Cadastrar-se"></UserForm>
+        <div>
+            <SectionHome/>
+            <SectionInfo/>
+            <SectionAbout/>
         </div>
-    )
+    );
 }
 
 export default Home;
