9d96c5e65ddb5c9b68c66723d6b6ea841ecb6299
Luis Felippe <luisfelippem@gmail.com>
2024-08-04 10:55:55 -0300

    sim

--- universitter/src/Components/Form/InputInfos1.jsx
+++ universitter/src/Components/Form/InputInfos1.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
 import styles from './UserForm.module.css'
 
 export default function InputInfos1({ name, value, placeholder, onChange }) {
 
 
   return (
-
-
     <input
       className={styles.fieldtext}
-      variant="filled"
       name={name}
       value={value}
       placeholder={placeholder}
--- universitter/src/Components/Form/InputInfos2.jsx
+++ universitter/src/Components/Form/InputInfos2.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
 import styles from './UserForm.module.css'
 
 export default function InputInfos2({ name, value, placeholder, onChange }) {
 
 
     return (
-
-
         <input
             className={styles.anothertext}
             name={name}
