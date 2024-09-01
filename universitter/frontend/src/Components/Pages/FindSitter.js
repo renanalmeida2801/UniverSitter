70e730968049add85fe886e433916fa90172b3f0
Luis Felippe <luisfelippem@gmail.com>
2024-09-01 14:10:38 -0300

    consertando o layout da tela de cadastro do cuidador

---
+++ universitter/frontend/src/Components/Pages/FindSitter.js
@@ -0,0 +1,16 @@
+import "./FindSitter.css";
+function FindSitter() {
+
+
+
+    return (
+        <div className="page">
+            <label>CONHEÇA ALGUNS DOS NOSSOS CUIDADORES</label>
+            <div className="container">
+
+            </div>
+        </div>
+    )
+
+}
+export default FindSitter
