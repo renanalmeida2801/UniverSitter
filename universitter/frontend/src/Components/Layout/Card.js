aa272ac12eff9b51e28c47d45b4eec9d7e0a68db
MarceloMikael <marcelo.mplp@hotmail.com>
2024-09-03 10:57:10 -0300

    Foi criado os cartões porém ainda falta acabar

--- universitter/frontend/src/Components/Layout/Card.js
+++ universitter/frontend/src/Components/Layout/Card.js
@@ -1,4 +1,22 @@
+import styles from './Card.module.css';
 const Card = (props) => {
-  <div widht={props.widht}>
-  </div>
-} 
+  return (
+    <div style={{ height: props.height}} className={styles.cardContainer}>
+      <div className={styles.imageContainer}>
+        <img src={props.img} />
+      </div>
+      <div className={styles.infoContainer}>
+        <div className={styles.infoHeader}></div>
+        <div className={styles.infoBody}>
+          <div className={styles.desc}>{props.descricao}</div>
+          <div className={styles.actions}>
+            <button className={styles.button}>Detalhes</button>
+            <button className={styles.button}>Chat</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
--- universitter/frontend/src/Components/Pages/FindSitter.js
+++ universitter/frontend/src/Components/Pages/FindSitter.js
@@ -1,31 +1,47 @@
 import { useState, useEffect, useCallback } from "react"
 import api from "../../services/api.ts"
+import Card from "../Layout/Card.js"
 
 import Styles from './FindSitter.module.css'
 
 const FindSitter = () => {
-    const [data, setData] = useState([
-        {
-            name: "Luigy",
-            starLevel: 5,
-            sitterType: ["Pet", "Plant"],
-            desc: "A lovely mother fucker"
-        }
-    ])
+  const [data, setData] = useState([
+    {
+      name: "Luigy",
+      starLevel: 5,
+      sitterType: ["Pet", "Plant"],
+      descricao: "A"
+    },
+    {
+      name: "Luigy",
+      starLevel: 5,
+      sitterType: ["Pet", "Plant"],
+      descricao: "A"
+    },
+    {
+      name: "Luigy",
+      starLevel: 5,
+      sitterType: ["Pet", "Plant"],
+      descricao: "A"
+    }
+  ])
 
-    // useCallback(async () => {
-    //   api.get('/sitters').then(
-    //     setData(data)
-    //   )
-    // })
+  // useCallback(async () => {
+  //   api.get('/sitters').then(
+  //     setData(data)
+  //   )
+  // })
 
-    return (
-        <div className={Styles.container}>
-            <h1>Veja alguns dos nossos cuidadores!</h1>
-            <div className={Styles.sittersContainer}>
-            </div>
-        </div>
-    )
+  return (
+    <div className={Styles.container}>
+      <h1 className={Styles.h1}>Veja alguns dos nossos cuidadores!</h1>
+      <div className={Styles.sittersContainer}>
+        {data.map((item) => {
+          return <Card height='200px' width='400px' {...item} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 
