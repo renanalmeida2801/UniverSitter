ea8c844dbf446e037853faf4620ed3f1bb7823dc
Luis Felippe <luisfelippem@gmail.com>
2024-09-04 20:41:29 -0300

    arrumando a tela de login

--- universitter/frontend/src/Components/Layout/SectionAbout.js
+++ universitter/frontend/src/Components/Layout/SectionAbout.js
@@ -6,7 +6,7 @@ import image from '../../img/plantaQuemSomos.jpg'
 function SectionAbout() {
     return (
         <div className={styles.container}>
-            <div>
+            <div className={styles.div_text}>
                 <h2 className={styles.text}>Quem somos?</h2><br></br>
                 <p className={styles.info}>O UniverSitter é um projeto de alunos da UFC - Quixadá</p><br></br>
                 <p className={styles.info}>Nossa ideia é conectar os unversitários do campus e fortalecer a comunidade através de uma troca de favores!</p><br></br>
@@ -15,7 +15,7 @@ function SectionAbout() {
                 <Link className={styles.button} to='/'>Tornar-se um cuidador</Link>
 
             </div>
-            <div>
+            <div className={styles.div_img}>
                 <img className={styles.img} src={image} alt='vaso com plantas'></img>
             </div>
         </div>
