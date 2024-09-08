e66b80b53795b51808a32d0dfd6c910e5dfefa0a
MarceloMikael <marcelo.mplp@hotmail.com>
2024-09-08 20:35:01 -0300

    Alterado a lógica por trás dos comentários de feedback

--- universitter/frontend/src/Components/Layout/InfoSitter/SitterReviews.js
+++ universitter/frontend/src/Components/Layout/InfoSitter/SitterReviews.js
@@ -6,34 +6,34 @@ import { date } from 'zod'
 
 
 function SitterReviews(props) {
-    const newDate = new Date(props.reviewDate);
-    const formatedDate = newDate.toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    })
+  const newDate = new Date(props.reviewDate);
+  const formatedDate = newDate.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  })
 
-    return (
-        <div className={Styles.container}>
+  return (
+    <div className={Styles.container}>
 
-            <div className={Styles.review}>
-                {props.sitterImg ? <img className={ProfilleImage} src={props.sitterImg}></img> : <img className={ProfilleImage} src={ProfilleImage}></img>}
-                <div className={Styles.infos}>
-                    <h1>{props.userName}</h1>
-                    <div className={Styles.star}>
-                        {
-                            Array.from({ length: props.reviewRating }).map((star, index) => {
-                                return <FaStar color='gold' size={30} />
-                            })
-                        }
-                    </div>
-                </div>
-
-                <p>{formatedDate}</p>
-                <h3>{props.sitterComment}</h3>
-            </div>
+      <div className={Styles.review}>
+        {props.sitterImg ? <img className={ProfilleImage} src={props.sitterImg}></img> : <img className={ProfilleImage} src={ProfilleImage}></img>}
+        <div className={Styles.infos}>
+          <h1>{props.userName}</h1>
+          <div className={Styles.star}>
+            {
+              Array.from({ length: props.reviewRating }).map((star, index) => {
+                return <FaStar color='gold' size={30} />
+              })
+            }
+          </div>
+          <h3>{props.sitterComment}</h3>
         </div>
-    )
+
+        <p>{formatedDate}</p>
+      </div>
+    </div>
+  )
 }
 
 export default SitterReviews
--- universitter/frontend/src/Components/Pages/InfoSitter.js
+++ universitter/frontend/src/Components/Pages/InfoSitter.js
@@ -7,52 +7,51 @@ import { useState, useEffect, useRef } from "react";
 import api from "../../services/api.ts";
 
 function InfoSitter() {
-    const params = useParams()
-    const [data, setData] = useState({});
-    const [review, setReview] = useState({});
+  const params = useParams()
+  const [data, setData] = useState({});
+  const [review, setReview] = useState({});
 
-    const loadSitter = async () => {
-        try {
-            const response = await api.get(`/sitters/${params.id}`);
-            setData(response.data.data)
-            //   console.log(response)
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+  const loadSitter = async () => {
+    try {
+      const response = await api.get(`/sitters/${params.id}`);
+      setData(response.data.data)
+      //   console.log(response)
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-    const loadfeedbacks = async () => {
-        try {
-            const response = await api.get(`/feedback/${params.id}`);
-            setReview(response.data)
-            console.log(response)
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+  }
+  const loadfeedbacks = async () => {
+    try {
+      const response = await api.get(`/feedback/${params.id}`);
+      setReview(response.data)
+      console.log(response)
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  }
 
-    useEffect(() => {
-        loadSitter();
-        loadfeedbacks();
-        console.log("CHamou")
-        // console.log(data)
-    }, []);
+  useEffect(() => {
+    loadSitter();
+    loadfeedbacks();
+    // console.log(data)
+  }, []);
 
 
 
-    return (
-        <div className={Styles.container}>
-            <Sitter sitterName={data.nome} sitterLastName={data.sobrenome} sitterRating={Number(data.rating).toPrecision(1)} sitterImage={data.foto} sitterCategory={data.categoria} sitterAddress={data.endereco} />
-            <SitterDescription sitterDescription={data.descricao} />
-            <h1 className={Styles.container_h1}>Avaliações</h1>
-            <div className={Styles.commentContainer}>
-                {review.data && review.data.map((item) => (
+  return (
+    <div className={Styles.container}>
+      <Sitter sitterName={data.nome} sitterLastName={data.sobrenome} sitterRating={Number(data.rating).toPrecision(1)} sitterImage={data.foto} sitterCategory={data.categoria} sitterAddress={data.endereco} />
+      <SitterDescription sitterDescription={data.descricao} />
+      <h1 className={Styles.container_h1}>Avaliações</h1>
+      <div className={Styles.commentContainer}>
+        {review.data && review.data.map((item) => (
 
-                    <SitterReviews sitterComment={item.comment} reviewDate={item.date} reviewRating={item.rating} userName={item.nome} sitterImg={item.foto} />
-                    // Add a unique key for each item
-                ))}
-            </div>
-        </div>
-    )
+          <SitterReviews sitterComment={item.comment} reviewDate={item.date} reviewRating={item.rating} userName={item.nome} sitterImg={item.foto} />
+          // Add a unique key for each item
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default InfoSitter;
